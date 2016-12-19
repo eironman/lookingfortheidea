d5468bf6d232bd1e47f1bd5a1a7b2b572148453d
Aaron Amengual Arranz <aaron.amengual@gmail.com>
2016-12-19 12:51:51 +0100

    Added mailgun. Page intro responsive.

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from .models import ContactMessage
+
 
 
 def contact(request):
@@ -29,16 +30,18 @@ def message(request):
             email=email,
             message=message
         )
-        contact_message.sa
+        contact_message.save()
 
         # Send message
-        send_mail(
+        email = EmailMessage(
             '[Buscando La Idea] Mensaje de ' + name,
             'Correo: %s\n\nMensaje:\n%s' % (email, message),
             'aaron.amengual@gmail.com',
             ['aaron.amengual@gmail.com'],
-            fail_silently=False,
+            reply_to=[email]
         )
+        email.send()
+
         context = {
             'success_message': "¡Muchas gracias por tu mensaje!"
         }
