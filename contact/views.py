0846a6bb7c431dd2a20e6fc73ff687ff3993b02f
Aaron Amengual Arranz <aaron.amengual@gmail.com>
2016-12-20 11:23:52 +0100

    Fixed email bug

--- contact/views.py
+++ contact/views.py
@@ -12,13 +12,13 @@ def contact(request):
 def message(request):
     """Contact message"""
     name = request.POST['name']
-    email = request.POST['email']
+    user_email = request.POST['email']
     message = request.POST['message']
 
     if not name or not message:
         context = {
             'name': name,
-            'email': email,
+            'email': user_email,
             'message': message,
             'error_message': "¡Ups! ¿Podrías decirme tu nombre y el mensaje por favor?"
         }
@@ -27,17 +27,21 @@ def message(request):
         # Store message
         contact_message = ContactMessage(
             name=name,
-            email=email,
+            email=user_email,
             message=message
         )
         contact_message.save()
 
         # Send message
+        recipients = ['info@buscandolaidea.com']
+        if user_email:
+            recipients.append(user_email)
+
         email = EmailMessage(
             '[Buscando La Idea] Mensaje de ' + name,
             message,
             'info@buscandolaidea.com',
-            ['info@buscandolaidea.com', email],
+            recipients,
             reply_to=['info@buscandolaidea.com']
         )
         email.send()
