a7d77790e6a30359711ba62407a1b9d25d8c2448
Aaron Amengual Arranz <aaron.amengual@gmail.com>
2017-02-22 09:47:38 -0400

    Autofill the author of a comment

--- blog/static/blog/js/content/Comment.js
+++ blog/static/blog/js/content/Comment.js
@@ -1,21 +1,5 @@
 var Comment = {
 
-	// Security checks before sending comment
-    validateCommentForm: function(form)
-    {
-        var commentAuthor = form.find('input[name="comment_author"]').val();
-        var commentContent = form.find('textarea').val();
-        if (commentAuthor == '' || commentContent == '') {
-            // Remove previous error message
-            form.find('.text-color-a').remove();
-
-            // Show error message
-            var errorMessage = '<p class="mb10 text-color-a">Completa todos los campos por favor</p>';
-            form.find('input[type="hidden"]').first().before(errorMessage);
-            return false;
-        }
-    },
-
     // Creates the reply form for a comment
     appendReplyForm: function(replyButton)
     {
@@ -41,9 +25,38 @@ var Comment = {
         replyButton.hide();
     },
 
+    // Fills the author field with the previous name used
+    fillCommentAuthor: function()
+    {
+        var commentAuthor = Storage.get('bli_comment_author');
+        if (!Helper.isEmpty(commentAuthor)) {
+            $('.comment_form').find('input[name="comment_author"]').val(commentAuthor);
+        }
+    },
+
+	// Security checks before sending comment
+    validateCommentForm: function(form)
+    {
+        var commentAuthor = form.find('input[name="comment_author"]').val();
+        var commentContent = form.find('textarea').val();
+        if (commentAuthor == '' || commentContent == '') {
+            // Remove previous error message
+            form.find('.text-color-a').remove();
+
+            // Show error message
+            var errorMessage = '<p class="mb10 text-color-a">Completa todos los campos por favor</p>';
+            form.find('input[type="hidden"]').first().before(errorMessage);
+            return false;
+        }
+
+        Storage.set('bli_comment_author', commentAuthor)
+    },
+
     init: function()
     {
         var self = this;
+
+        this.fillCommentAuthor();
 
         // Check mandatory fields for comment
         $('.container').on('submit', '.comment_form', function() {
